465dbe054b96584f062342f55a6e935c0aac5dfe
jbreeden <jared.breeden@gmail.com>
2016-01-18 08:46:30 -0800

    Describe rake tasks

--- Rakefile.rb
+++ Rakefile.rb
@@ -1,9 +1,11 @@
 GLIB_HOME = "/usr/local/Cellar/glib/2.46.2"
 
+desc 'Run the low-level binding tests'
 task :test do
   sh 'mruby test/test_bindings.rb'
 end
 
+desc 'Run the specs'
 task :specs do
   Dir['specs/*'].each do |spec|
     system "mruby #{spec}"
@@ -12,10 +14,12 @@ end
 
 namespace :api do
   run = proc { |ruby_cmd| system "echo \"puts ERB.new(File.read('api.erb'), nil, '-').result\" | #{ruby_cmd}" }
+  
   desc 'Print the MRuby APIs'
   task :mruby do
     run["mruby"]
   end
+  
   desc 'Print the CRuby APIs'
   task :cruby do
     run["ruby -r erb"]
